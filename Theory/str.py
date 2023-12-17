09e3472ac1cf8f7bc572d31633e2bc707170e5f3
Yuliia Bondarenko <bondarenko047@gmail.com>
2023-12-17 22:16:32 +0200

    str

---
+++ Theory/str.py
@@ -0,0 +1,33 @@
+my_name = 'Yuliia'
+print(my_name)
+
+greeting = "Hello from Python!"
+print(greeting)
+
+info_msg = """"You are
+learning the eastiest
+programming language -
+Python!"""
+print(info_msg)
+
+# Встроенные функции и строки
+print(len(my_name))   # длина строки
+print(my_name[0])   # символ строки по индексу
+print(my_name[2:4])   # диапазон символов
+
+
+# МЕТОДЫ СТРОК
+my_name.upper()
+# my_name.replace()  # замена часть строки на другую часть
+print(greeting.replace('Hello', 'Hi'))
+# my_name.count()   # счетчик символа или слова
+print(greeting.count('l'))
+# my_name.index()   # узнать индекс определенного символа в строке
+my_name.capitalize()
+my_name.lower()
+print(my_name[0])
+print(my_name[-1])
+print(my_name[1:8])
+print(my_name[2:])
+print(my_name[:5])
+print(my_name[:])
