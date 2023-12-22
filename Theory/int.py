afa5803a30d116aa5a11ef85c7261cc12ae30b4d
Yuliia Bondarenko <bondarenko047@gmail.com>
2023-12-22 23:04:37 +0200

    num

--- Theory/int.py
+++ Theory/int.py
@@ -21,3 +21,27 @@ print(type(result))
 average_price = 17.25
 print(average_price)
 print(type(average_price))
+
+print(int(average_price))
+
+str_temperature = '14.5'
+temperature = float(str_temperature)
+print(temperature)
+print(type(temperature))
+
+# ОКРУГЛЕНИЕ ЧИСЕЛ
+
+rate = 18.08
+print(round(rate))
+print(type(rate))
+print(type(round(rate)))
+
+# КОМПЛЕКСНЫЕ ЧИСЛА
+# Комплексное число состоит из действительной и мнимой частей
+
+complex_a = 3 + 5j
+complex_b = 4 + 7j
+
+sum = complex_a + complex_b   # + - * /
+print(sum)
+print(type(sum))
