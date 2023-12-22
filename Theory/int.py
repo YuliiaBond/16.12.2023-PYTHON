b7a649677c3fb0f2de4d4461b9c3b35f4765754e
Yuliia Bondarenko <bondarenko047@gmail.com>
2023-12-22 22:33:48 +0200

    int

--- Theory/int.py
+++ Theory/int.py
@@ -1,1 +1,23 @@
-# видео 38
+num = 10
+
+print(type(num))
+
+# any_num = input("Enter any number: ")
+# # или
+# # any_num = int(input("Enter any number: "))
+# print(any_num)
+# print(type(any_num))   # STR
+# any_num2 = int(any_num)
+# print(type(any_num2))  # INT
+
+# ВОЗВЕДЕНИЕ В СТЕПЕНЬ
+base = 5
+power = 3
+result = pow(base, power)  # 125
+print(type(result))
+
+# ЧИСЛА С ДЕСЯТИЧНОЙ ТОЧКОЙ
+
+average_price = 17.25
+print(average_price)
+print(type(average_price))
