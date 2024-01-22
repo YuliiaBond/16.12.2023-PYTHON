434d641d91ffe05907f12119268241f5f0bfa7de
Yuliia Bondarenko <bondarenko047@gmail.com>
2024-01-22 17:35:09 +0200

    d2

--- Theory/dict.py
+++ Theory/dict.py
@@ -1,4 +1,4 @@
-# СЛОВАРЬ - набор элементов ключ:значение
+# СЛОВАРЬ - набор элементов (ключ:значение)
 # Порядок элементов в словаре НЕ имеет значения (индексов нет)
 my_motorbike = {
     'brand': 'Ducati',
@@ -12,8 +12,40 @@ other_motorbike = {
 }
 print(my_motorbike)
 print(other_motorbike)
-print(my_motorbike == other_motorbike)
-print(id(my_motorbike) == id(other_motorbike))
+print(my_motorbike == other_motorbike)   # технически одинаковые словари
+print(id(my_motorbike) == id(other_motorbike))   # разные обьекты
+
+# Получение значения в []
+print(my_motorbike['brand'])
+
+# Изменение значений
+my_motorbike['price'] = 2000
+print(my_motorbike['price'])
+
+# Добавление новых элементов
+my_motorbike['is_new'] = True
+print(my_motorbike)
 
 # Удаление элементов
-# видео 55
+del my_motorbike['engine_vol']
+print(my_motorbike)
+
+# Доступ к значению элемента с помощью переменной
+my_bike = {
+    'brand': 'Ducati',
+    'price': 25000,
+    'engine_vol': 1.2,
+}
+key_name = 'brand'
+my_bike[key_name] = 'BMW'
+print(my_bike)
+
+# Вложенные словари
+my_motorbike2 = {
+    'brand': 'Ducati',
+    'price_info': {
+        'price': 10000,
+        'is_available': True},
+    'engine_vol': 1.2,
+}
+print(my_motorbike2['price_info']['price'])
